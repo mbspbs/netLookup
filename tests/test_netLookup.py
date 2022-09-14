d365af799edfd04a7ce0e33600b7d1d193a13adb
W. Nick Pappin <npappin@mbspbs.com>
2022-09-14 14:43:00 -0700

    Check the add logic for v6.

--- tests/test_netLookup.py
+++ tests/test_netLookup.py
@@ -13,6 +13,15 @@ def test_Table1():
 def test_Table2():
     table = netLookup.Table()
     entry = {
+        'network': ipaddress.ip_network('2600:100f:b000::/44'),
+        'data': 'stuff'
+    }
+    table.add(entry)
+    pass
+
+def test_Table3():
+    table = netLookup.Table()
+    entry = {
         'network': 'foo',
         'data': 'stuff'
     }
