8bb5bcc20517926a6907799814d6be9f3466c4a4
W. Nick Pappin <npappin@mbspbs.com>
2022-09-14 16:16:27 -0700

    Added negative test for query.

--- tests/test_netLookup.py
+++ tests/test_netLookup.py
@@ -39,3 +39,15 @@ def test_Query1():
     data = table.query(ip)
     data == entry
     pass
+
+def test_Query2():
+    table = netLookup.Table()
+    entry = {
+        'network': ipaddress.ip_network('2600:100f:b000::/44'),
+        'data': 'stuff'
+    }
+    table.add(entry)
+    ip = ipaddress.ip_address('2700:100f:b000:0100::100')
+    with pytest.raises(ValueError):
+        data = table.query(ip)
+    
