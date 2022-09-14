a5974b91481948a73fd7b70e9b447fee73f9771e
W. Nick Pappin <npappin@mbspbs.com>
2022-09-14 14:04:06 -0700

    Added add logic for inserting network and arbitrary data to table.

--- netLookup/netLookup.py
+++ netLookup/netLookup.py
@@ -18,10 +18,31 @@ class Table:
         for number in range(64,12,-1):
             self.lookupTable['v6'][number] = list()
     
-    def add(self, data: dict) -> bool:
-        print("Not yet implemented.")
+    def add(self, entry: dict) -> bool:
+        """Adds entry to lookup table.
 
-        pass
+        Args:
+            entry (dict): Must contain two keys, network and data.
+                Keys:
+                network: must be a ipaddress.ip_network variable
+                data: arbitrary data
+
+        Returns:
+            bool: Returns true if successful, and false if there is an issue.
+        """
+
+        assert isinstance(
+            entry["network"], (ipaddress.IPv4Network, ipaddress.IPv6Network)
+        ), False
+        version = entry["network"].version
+        version = f"v{version}"
+        netmask = entry['network'].prefixlen
+
+        try:
+            self.lookupTable[version][netmask].append((entry["network"],entry['data']))
+            return True
+        except:
+            return False
     
     def query(self, ip: ipaddress.ip_address) -> dict:
         print("Not yet implemented.")
