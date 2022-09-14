4c6e4067bf97bbcd9878c3785f7ea5d8f06f0fda
W. Nick Pappin <npappin@mbspbs.com>
2022-09-14 14:06:43 -0700

    Added tests to assure table works and assertion works.

---
+++ tests/test_netLookup.py
@@ -0,0 +1,21 @@
+import ipaddress, pytest
+from netLookup import netLookup
+
+def test_Table1():
+    table = netLookup.Table()
+    entry = {
+        'network': ipaddress.ip_network('192.168.1.0/24'),
+        'data': 'stuff'
+    }
+    table.add(entry)
+    pass
+
+def test_Table2():
+    table = netLookup.Table()
+    entry = {
+        'network': 'foo',
+        'data': 'stuff'
+    }
+    with pytest.raises(AssertionError):
+        table.add(entry)
+    
