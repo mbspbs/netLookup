09767896f475b6d3bf6ea0d89573b3b28c7b89ac
W. Nick Pappin <npappin@mbspbs.com>
2022-10-12 09:18:57 -0700

    Tests with test data ignored.

--- tests/test_netLookup.py
+++ tests/test_netLookup.py
@@ -50,4 +50,26 @@ def test_Query2():
     ip = ipaddress.ip_address('2700:100f:b000:0100::100')
     with pytest.raises(ValueError):
         _ = table.query(ip)
-    
+
+def test_Load1():
+    table = netLookup.Table()
+    table.load()
+    ip = ipaddress.ip_address('2700:100f:b000:0100::100')
+    with pytest.raises(ValueError):
+        _ = table.query(ip)
+
+def test_Load2():
+    table = netLookup.Table()
+    table.load()
+    ip = ipaddress.ip_address('2600:100f:b000:0100::100')
+    _ = table.query(ip)
+
+def test_Save1():
+    table = netLookup.Table()
+    entry = {
+        'network': ipaddress.ip_network('2600:100f:b000::/44'),
+        'data': 'stuff'
+    }
+    table.add(entry)
+    table.save()
+    pass
